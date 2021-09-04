a96bcb044978fa55d98f0948f40f72ee23a02fb4
Maximilian Betz <maximilian.betz@gmail.com>
2021-09-04 22:57:57 +0200

    Switched from wikipedia to wikipediapi

--- wiki/wiki_harvester.py
+++ wiki/wiki_harvester.py
@@ -1,4 +1,4 @@
-import wikipedia
+import wikipediaapi
 import sys
 import datetime
 import argparse
@@ -10,56 +10,54 @@ verbose_output = False
 
 # Recursively loads all sub pages until the depth is 0 (basecase)
 def load_recursive(page, queue, depth=2, scanned=set()):
-    if depth <= 0 or (page in scanned and depth == 1):
+    page_title = page.title
+    if isinstance(page_title, list):
+        page_title = page_title[0]
+
+    if depth <= 0 or (page_title in scanned and depth == 1):
         return scanned
-    print(f"Start recursive loading from page {page}")
-    scanned.add(page)
+    # if verbose_output:
+    print(f"Start recursive loading from page {page_title}")
+    scanned.add(page_title)
 
-    try:
-        loaded_page = wikipedia.page(page)
-        for sub_page in loaded_page.links:
-            scanned = load_recursive(sub_page, queue, depth - 1,  scanned)
+    links = page.links
+    for title in sorted(links.keys()):
+        scanned = load_recursive(links[title], queue, depth - 1,  scanned)
 
-        content = loaded_page.content
-        if content:
-            queue.put({"depth":depth, "page":page, "content":loaded_page.content}, block=True, timeout=None)
-
-    except wikipedia.DisambiguationError:
-        scanned = load_recursive_by_search(page, queue, depth - 1,  scanned)
-    except wikipedia.PageError:
-        return scanned
+    content = page.text
+    if content:
+        queue.put({"depth":depth, "page":page_title, "content":content}, block=True, timeout=None)
 
     return scanned
 
-
-# Performs the query and starts the recursive search for the first or all of the returned items. Depending on the parameters.
-def load_recursive_by_search(query, process_all_results, queue, depth=1, scanned = set()):
-    search_result = wikipedia.search(query)
-    if not process_all_results:
-        search_result = search_result[:1]
-    for result in search_result:
-        scanned = load_recursive(result, queue, depth,  scanned)
-    return scanned
-
-# Starts the search and scraping on all of the supplied queries
-def search_process(queries, process_all_results, queue, depth):
+# Starts the scraping at the supplied pages
+def scraping_process(pages, queue, depth):
+    if verbose_output:
+        print(f"start scriping {pages}")
     scanned = set()
-    print("pages found: %s"%(scanned))
-    for query in queries:
-        print("starting with %s"%(query))
-        scanned = load_recursive_by_search(query, process_all_results, queue, depth, scanned)
-        print("%i sites have been scanned"%(len(scanned)))
+    for page in pages:
+        if verbose_output:
+            print("starting with %s"%(page.title))
+        scanned = load_recursive(page, queue, depth, scanned)
+        if verbose_output:
+            print("%i sites have been scanned"%(len(scanned)))
     queue.put(None, block=True, timeout=None)
 
 
 def main(args):
-    wikipedia.set_lang(args.language)
-    wikipedia.set_rate_limiting(True, min_wait=datetime.timedelta(0, 0, 50000))
+    wikipedia = wikipediaapi.Wikipedia(language = args.language,
+        extract_format = wikipediaapi.ExtractFormat.WIKI, timeout=args.timeout)
+    page = wikipedia.page(args.page)
+    if verbose_output and not page.exists():
+        print(f"page {args.page} does not exist")
+        quit()
+
+    # wikipedia.set_rate_limiting(True, min_wait=datetime.timedelta(0, 0, 50000))
     max_queue_size = 100
     page_queue = Queue(maxsize=max_queue_size)
 
     page_queue = Queue(maxsize=max_queue_size)
-    p = Process(target=search_process, args=([args.query],args.all,page_queue,args.depth,))
+    p = Process(target=scraping_process, args=([page],page_queue,args.depth,))
     p.start()
 
     counter = collections.Counter()
@@ -70,8 +68,8 @@ def main(args):
         else:
             counter.update(document['content'].split())
 
-    if args.s:
-        for line in counter.most_common(args.s):
+    if hasattr(args, 'size'):
+        for line in counter.most_common(args.size):
             word, _ =  line
             args.o.write("%s\n" % word)
     else:
@@ -84,12 +82,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser('''Query data from wikipedia by supplying a query, depth and source language''')
     parser.add_argument('-d', '--depth', default=2, type=int, help='Query depth. Runtime and output size increase exponentially with it.')
     parser.add_argument('-l', '--language', default='de', type=str, help='Languages to query e.g. en')
-    parser.add_argument('-a', '--all', default=False, action='store_true', help='Process all search result instead of only the first one')
     parser.add_argument('-s', '--size', type=int, help='Output the n most common words')
     parser.add_argument('-c', default=True, action='store_true', help='Cleanup punctation in output')
-    parser.add_argument('-v', default=False, action='store_true', help='Verbose output, polutes stdout')
+    parser.add_argument('-v', action='store_true', help='Verbose output, polutes stdout')
+    parser.add_argument('-t', '--timeout', default=30, type=int, help='Timeout of the requests to wikipedia')
     parser.add_argument('-o', type=argparse.FileType('w',encoding="utf-8"), default=sys.stdout, help='Output file')
-    parser.add_argument('query', nargs=1, help='Query to run')
+    parser.add_argument('page', nargs=1, help='Page to start processing from')
 
     args = parser.parse_args()
     verbose_output = args.v
